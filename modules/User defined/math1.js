c0595fef382e134dc98d967d4042261cffa746e8
shoaib3474 <shoaib3974@gmail.com>
2025-04-03 22:38:55 +0500

    concept of module

---
+++ modules/User defined/math1.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+
+
+
+function sub(a, b) {
+    return a - b;
+}
+
+//ecports multiple function at one time 
+module.exports = {
+    add,
+    sub
+}
---
+++ modules/User defined/script.js
@@ -0,0 +1,10 @@
+const math = require('./math.js')
+
+//math1 is different to use 
+const math1 = require('./math1.js')
+
+
+console.log(math(3, 3))
+console.log(math1.add(32, 32))
+
+
