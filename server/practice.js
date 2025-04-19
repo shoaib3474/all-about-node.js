f5787bc81add36b2f01120f69d06498a772ffadd
shoaib3474 <shoaib3974@gmail.com>
2025-04-19 23:53:21 +0500

    creating login and signup response

--- server/practice.js
+++ server/practice.js
@@ -2,32 +2,73 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const myserver = http.createServer((req, res) => {
+const webServer = http.createServer((req, res) => {
 
+    if (req.url === '/login') {
+        const filePath = path.join(__dirname, 'login.html')
 
-
-    if (req.url === '/') {
-
-
-
-
-        const filePath = path.join(__dirname, 'login.html');
 
         fs.readFile(filePath, (err, data) => {
             if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                return res.end('Error loading the HTML file');
+                res.end(`Bhi masla ho gya hai! 😔😔😔😭 ${err} `)
+            } else {
+                res.end(data)
             }
+        })
+    } else if (req.url === '/signup') {
+        const filePath = path.join(__dirname, 'signup.html')
 
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.end(`Bhi masla ho gya hai! 😔😔😔😭 ${err} `)
+            } else {
+                res.end(data)
+            }
+        })
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
+        res.end(`Bhi masla ho gya hai! 😔😔😔😭  `)
     }
-});
 
-myserver.listen(8100, () => {
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myserver = http.createServer((req, res) => {
+
+
+
+//     if (req.url === '/') {
+
+//         const filePath = path.join(__dirname, 'login.html');
+
+//         fs.readFile(filePath, (err, data) => {
+//             if (err) {
+//                 res.writeHead(500, { 'Content-Type': 'text/plain' });
+//                 return res.end('Error loading the HTML file ');
+//             }
+
+//             res.writeHead(200, { 'Content-Type': 'text/html' });
+//             res.end(data);
+//         });
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         res.end('404 Not Found');
+//     }
+// });
+
+webServer.listen(8100, () => {
     console.log('URL: http://localhost:8100');
 });
